f5a3ccffe528dbac1e296cdc311e309cbe0d40ab
ehab-dabous <bmind58@gmail.com>
2022-07-07 18:33:10 +0300

    clean_environment module

--- clean_environment.py
+++ clean_environment.py
@@ -1,14 +1,24 @@
 import requests
 import json
 
-def server_stop(servername ):
+def backend_stop(servername ):
     req=requests.get(servername)
     print(req)
     if req.ok:
         print(req.json())
 
-#urlfrontend='http://127.0.0.1:5001/stop_server'
+
 
 urlbackend='http://127.0.0.1:5000/stop_server'
-server_stop(urlbackend)
-# server_stop(urlfrontend)
+backend_stop(urlbackend)
+
+
+
+def frondend_stop(servername ):
+    req=requests.get(servername)
+    print(req)
+    if req.ok:
+        print(req.json())
+
+urlfrontend='http://127.0.0.1:5001/stop_server'
+frondend_stop(urlfrontend)
