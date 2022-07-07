bb3c8948e43861782b94611587c0cee25496f2b6
ehab-dabous <bmind58@gmail.com>
2022-07-07 11:17:00 +0300

    clean_environment module

---
+++ clean_environment.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def server_stop(servername ):
+    req=requests.get(servername)
+    print(req)
+    if req.ok:
+        print(req.json())
+
+urlfrontend='http://127.0.0.1:5001/stop_server'
+urlbackend='http://127.0.0.1:5000/stop_server'
+server_stop(urlbackend)
+server_stop(urlfrontend)
