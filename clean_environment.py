653aab43cf5f5a32595351be25e89d2a8f9e9601
ehab-dabous <bmind58@gmail.com>
2022-07-07 18:27:44 +0300

    clean_environment module

--- clean_environment.py
+++ clean_environment.py
@@ -7,7 +7,8 @@ def server_stop(servername ):
     if req.ok:
         print(req.json())
 
-urlfrontend='http://127.0.0.1:5001/stop_server'
+#urlfrontend='http://127.0.0.1:5001/stop_server'
+
 urlbackend='http://127.0.0.1:5000/stop_server'
 server_stop(urlbackend)
-server_stop(urlfrontend)
+# server_stop(urlfrontend)
