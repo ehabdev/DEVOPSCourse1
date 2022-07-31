6597185341a2fb788792e9e597754c3ff1084e82
ehab-dabous <bmind58@gmail.com>
2022-07-31 10:12:39 +0300

    clean_environment update

--- clean_environment.py
+++ clean_environment.py
@@ -14,11 +14,11 @@ backend_stop(urlbackend)
 
 
 
-def frondend_stop(servername ):
-    req=requests.get(servername)
-    print(req)
-    if req.ok:
-        print(req.json())
-
-urlfrontend='http://127.0.0.1:5001/stop_server'
-frondend_stop(urlfrontend)
+# def frondend_stop(servername ):
+#     req=requests.get(servername)
+#     print(req)
+#     if req.ok:
+#         print(req.json())
+#
+# urlfrontend='http://127.0.0.1:5001/stop_server'
+# frondend_stop(urlfrontend)
